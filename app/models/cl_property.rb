e4ba1df2e02ebc4374c5ba351a220b168ce1268f
David Hsu <dvdhsu@gmail.com>
2014-09-17 00:56:10 -0700

    Dependent destroy for ClProperty.
    ClImages, ClAnnotation, and ClLocation are destroyed when a
    ClProperty is destroyed.

--- app/models/cl_property.rb
+++ app/models/cl_property.rb
@@ -1,5 +1,5 @@
 class ClProperty < ActiveRecord::Base
-  has_one :cl_annotation
-  has_one :cl_location
-  has_many :cl_images
+  has_one :cl_annotation, dependent: :destroy
+  has_one :cl_location, dependent: :destroy
+  has_many :cl_images, dependent: :destroy
 end
