a9799572bfabddf1c61d7fe2998f1da1dd609ee0
David Hsu <dvdhsu@gmail.com>
2014-10-22 15:53:01 +0100

    Add reverse geocoding for ClLocation via Ruby Geocoder.

--- app/models/cl_location.rb
+++ app/models/cl_location.rb
@@ -1,3 +1,6 @@
 class ClLocation < ActiveRecord::Base
   belongs_to :cl_property
+
+  reverse_geocoded_by :latitude, :longitude, :address => :geocoded_address
+  after_validation :reverse_geocode
 end
