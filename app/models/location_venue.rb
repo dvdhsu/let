a8725838311f592ce883fd5e4ed9a8036c5de191
David Hsu <dvdhsu@gmail.com>
2014-10-22 21:06:45 +0100

    Add LocationVenue model.
    This keeps tracks of venues that are close to locations. Examples
    include grocery stores, restaurants, and drugstores.

---
+++ app/models/location_venue.rb
@@ -0,0 +1,3 @@
+class LocationVenue < ActiveRecord::Base
+  belongs_to :cl_location
+end
---
+++ db/migrate/20141022164243_create_location_venues.rb
@@ -0,0 +1,22 @@
+class CreateLocationVenues < ActiveRecord::Migration
+  def change
+    create_table :location_venues do |t|
+      t.references :cl_location, index: true
+      t.string :category
+      t.string :address
+      t.float :longitude
+      t.float :latitude
+      t.float :rating
+      t.string :name
+      t.string :source
+      t.string :external_id
+      t.string :external_url
+      t.string :external_image_url
+      t.string :phone
+      t.integer :distance
+
+      t.timestamps null: false
+    end
+    add_index :location_venues, :external_id
+  end
+end
